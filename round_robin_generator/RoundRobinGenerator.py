12958bda4c3c7e9d71c3f10c2876cd0e4de7c76d
Joe Badlato <joebadlato@gmail.com>
2020-04-26 16:22:00 -0400

    use enum to set alg implementation

--- round_robin_generator/RoundRobinGenerator.py
+++ round_robin_generator/RoundRobinGenerator.py
@@ -2,6 +2,7 @@ import collections
 import random
 import time
 from datetime import datetime
+from enum import Enum
 
 import pandas as pd
 from round_robin_generator.generate_matchups import generate_player_matchups
@@ -16,9 +17,29 @@ pd.set_option("display.max_columns", 500)
 pd.set_option("display.width", 1000)
 
 
+class MatchupImplementation(Enum):
+    CIRCLE = "CIRCLE"
+    DEFAULT_SCRAMBLE = "DEFAULT_SCRAMBLE"
+
+
 class RoundRobinGenerator:
-    def __init__(self):
+    """
+    :type implementation: MatchupImplementation
+    :param implementation: Sets the algorithm used to generate the round robin matchups. Default is the Circle method.
+    """
+
+    def __init__(self, matchup_implementation=MatchupImplementation.DEFAULT_SCRAMBLE):
         pass
+
+    def create_matchups(self, player_list, num_rounds):
+        return {
+            MatchupImplementation.CIRCLE: create_matchups_circle(
+                player_list, num_rounds
+            ),
+            MatchupImplementation.DEFAULT_SCRAMBLE: create_matchups_alternate(
+                player_list, num_rounds
+            ),
+        }[matchup_implementation]
 
     def create_matchups_circle(self, player_list, num_rounds):
         # Create matchups using the circle method
