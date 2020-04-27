fa95b395d31c820c68f63779a22aa41e33f1114d
Mike Stefkovich <mikes1@vt.edu>
2020-04-26 21:56:52 -0400

    Added some frameworks for round robins with odd number of teams.

--- tests/test_mike_round_robin.py
+++ tests/test_mike_round_robin.py
@@ -11,7 +11,6 @@ def test_default_scramble(num_rounds):
     Checking to ensure the output shape is valid. Also checks to make sure there are no
     duplicate matches.
     :param num_rounds: Number of rounds to run the round robin algorithm.
-    :return:
     """
     output = default_scramble(num_rounds, test_players)
     assert output.shape[0] >= 1
@@ -20,9 +19,22 @@ def test_default_scramble(num_rounds):
     assert len(np.unique(output.values)) == output.shape[0] * output.shape[1]
 
 
+def test_excess_rounds():
+    """
+    Test when the number of rounds is greater than the number of teams -1.
+    """
+    output = default_scramble(6, test_players, reshuffle_excess=False)
+    assert sorted(np.unique(output.loc[:'Round 3', :].values)) == sorted(np.unique(output.loc['Round 4':, :].values))
+
+    # The the case with two teams
+    o2 = default_scramble(2, ['Mike', 'Tim'], reshuffle_excess=False)
+    assert o2.loc['Round 1', 'Game 1'] == o2.loc['Round 2', 'Game 1']
+
 def test_invalid_round_numbers():
     with pytest.raises(Exception):
         default_scramble(0, test_players)
 
     with pytest.raises(Exception):
         default_scramble(2, ["Mike", "Joe", "Dan"])
+
+
