69e79eff5e453c01d7daca1b9d749bcbf9351c06
Mike Stefkovich <mikes1@vt.edu>
2020-04-28 17:30:43 -0400

    Implemented stagger matchup timing, no testing b/c yolo.

--- round_robin_generator/matchup_times.py
+++ round_robin_generator/matchup_times.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 
 def generate_times(matchup_df: pd.DataFrame, tournament_start_time, game_duration, game_stagger):
@@ -7,9 +7,20 @@ def generate_times(matchup_df: pd.DataFrame, tournament_start_time, game_duratio
     if game_stagger == 0:
         for round_num in range(time_df.shape[0]):
             round_key = 'Round ' + str(round_num + 1)
-            match_time = tournament_start_time + timedelta(minutes=(game_duration*round_num))
-            time_df.loc[round_key, :] = match_time.time().strftime('%H:%M%p')
+            match_time = tournament_start_time + timedelta(minutes=(game_duration * round_num))
+            time_df.loc[round_key, :] = match_time.strftime('%I:%M%p')
         return time_df
     else:
-        #TODO: Implement
-        pass
+        """
+        # Given the algorithm, at worst every player can play every (game duration + stagger time)
+        # This is b/c your opponent begins play one stagger count after you at the latest.
+        """
+        for round_num in range(time_df.shape[0]):
+            round_key = 'Round ' + str(round_num + 1)
+            default_spread = [tournament_start_time + timedelta(minutes=game_num * game_stagger) for game_num in
+                              range(time_df.shape[1])]
+            match_times = [
+                (def_time + timedelta(minutes=((game_duration + game_stagger) * round_num))).strftime('%I:%M%p') for
+                def_time in default_spread]
+            time_df.loc[round_key, :] = match_times
+        return time_df
